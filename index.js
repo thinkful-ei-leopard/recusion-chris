5f0f5b08df3dc52d0f069f679ed1442970c9e594
Christopher Floyd <cfloyd252@gmail.com>
2020-03-30 09:29:14 -0700

    completed drill 2: power calculator

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+
 const countingSheep = function(input) {
   if (input === 0) {
     return console.log('All sheep jumped over the fence');
@@ -7,4 +10,14 @@ const countingSheep = function(input) {
   countingSheep(input - 1);
 };
 
-countingSheep(3);
+const powerCalculator = function(base, exponent) {
+  if (exponent <= 0) {
+    return console.log('exponent should be >= 0');
+  } else if (exponent === 1) {
+    return console.log(base);
+  }
+
+  powerCalculator(base = base * base, exponent - 1);
+};
+
+powerCalculator(10,0);
